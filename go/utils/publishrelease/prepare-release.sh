d519307d7dd70f5987b53bb9d849891345a309b0
Oscar Batori <oscar@liquidata.co>
2020-12-19 19:02:42 -0800

    another fix

--- go/utils/publishrelease/prepare-release.sh
+++ go/utils/publishrelease/prepare-release.sh
@@ -17,14 +17,13 @@ git checkout -b $release_branch --track "origin/$release_branch"
 git merge -m "Merging latest master into $release_branch for release $version_str" master
 
 echo "Updating the version to $version"
-echo "script dir is $script_dir"
 sed -i '' -e 's/Version = ".*"/Version = "'"$version_str"'"/' $dolt_cmd_entry_point
 
 echo "Creating commit for $release_branch"
 git add $dolt_cmd_entry_point
 git commit -m "Updated version for release of version $version_str"
 
-ehco "Creating tag for version $version_str"
+echo "Creating tag for version $version_str"
 git tag -a $version_str -m "Tag for release of Dolt version $version_str"
 
 echo "Pushing $release_branch"
